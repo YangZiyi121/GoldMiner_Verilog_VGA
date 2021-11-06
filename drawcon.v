86fc551455f980b9a4d9fc1162a12a483c290e97
YangZiyi121 <89967414+YangZiyi121@users.noreply.github.com>
2021-11-06 16:18:51 +0300

    Merge hook moving logic and back ground figure logic

--- drawcon.v
+++ drawcon.v
@@ -21,18 +21,61 @@
 
 
 module drawcon(
-    input [7:0] W_rom_data,
+    input [7:0] W_rom_data, //for testing, comment this statement
     input [10:0] draw_x,
     input [9:0] draw_y,
+    input [10:0] blkpos_x,
+    input [9:0] blkpos_y,
     output [3:0] draw_r,
     output [3:0] draw_g,
     output [3:0] draw_b
     );
     
-
+    reg [3:0] bg_r, bg_g, bg_b;
+    reg [3:0] blk_r, blk_g, blk_b;
     
-    assign draw_r = (draw_x >=10 && draw_x<= 1269 && draw_y>=10 && draw_y <= 789)? W_rom_data[7:5]*2:4'b1111;
-    assign draw_g = (draw_x >=10 && draw_x<= 1269 && draw_y>=10 && draw_y <= 789)? W_rom_data[4:2]*2:4'b1111;
-    assign draw_b = (draw_x >=10 && draw_x<= 1269 && draw_y>=10 && draw_y <= 789)? W_rom_data[1:0]*4:4'b1111;
+    //Draw border and background
+    always @ *
+    begin
+    if (draw_x < 11'd10 | draw_x > 11'd1269 | draw_y < 10'd10 | draw_y > 10'd789)
+        begin
+            bg_r <=  4'b1111;
+            bg_g <=  4'b1111;
+            bg_b <=  4'b1111;
+        end
+    else 
+        begin
+            bg_r <= W_rom_data[7:5]*2;
+            bg_g <= W_rom_data[4:2]*2;
+            bg_b <= W_rom_data[1:0]*4;
+  /*for testing, use the below ones*/
+//            bg_r <=  4'b0000;
+//            bg_g <=  4'b0000;
+//            bg_b <=  4'b0000;
+        end
+    end
+
+    //Draw block
+    always @*
+    begin
+    if (draw_x >=blkpos_x & draw_x<=blkpos_x+11'd32 & draw_y >=blkpos_y & draw_y<=blkpos_y+10'd32)
+        begin
+            blk_r <= 4'b1111;
+            blk_g <= 4'b1111;
+            blk_b <= 4'b0000;
+        end
+    else 
+        begin
+            blk_r <= 4'b0000;
+            blk_g <= 4'b0000;
+            blk_b <= 4'b0000;
+        end
+    end
+    
+    //Assign Output
+    assign draw_r = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_r : bg_r;
+    assign draw_g = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_g : bg_g;
+    assign draw_b = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_b : bg_b;
+
     
 endmodule
--- game_top.v
+++ game_top.v
@@ -31,86 +31,34 @@ module game_top(
     wire [10:0]curr_x;
     wire [9:0]curr_y;
     wire [3:0] pix_r_or,  pix_g_or,  pix_b_or;
+    wire [10:0]blk_out_x;
+    wire [9:0] blk_out_y;
     wire clkout;
     reg [1:0] counterColumn = 2'd0;
     reg [1:0] counterRow = 2'd0;
     reg clkdraw = 1'b0;
-    reg [16:0] R_rom_addr = 17'd0;
+    wire [16:0] R_rom_addr;
     wire [7:0] W_rom_data;
     /* pixal clock instance */
     clk_wiz_0 pixClkIns (.clk_in1(clk), .clk_out1(clkout));
     
-    /* background image instance */
+    /* store background image instance */
+    //for testing, comment this instance
     blk_mem_gen_0 rom (.clka(clkout), .addra(R_rom_addr), .douta(W_rom_data));
     
     /* vga-out instance */
     vga_out vgainst (.clk(clkout), .rst(rst), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or),                  
     .pix_r(pix_r), .pix_g(pix_g), .pix_b(pix_b), .hsync(hsync), .vsync(vsync), .curr_x(curr_x), .curr_y(curr_y)); 
-          
-    drawcon drawEnvironment (.W_rom_data(W_rom_data), .draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or));
     
-    /*The counter to make the figure bigger *3  */
-    always @(posedge clkout) begin
-    if (rst) begin
-        counterColumn <= 2'd0; counterRow <= 2'd0;
-    end
-    else begin
-        if(counterRow == 2'd2)  counterRow = 2'd0;
-
-        else if (curr_x == 11'd10) counterRow = counterRow + 1'b1;
-           
-        if(counterColumn == 2'd2)  begin
-            counterColumn = 2'd0;
-        end 
-        else if(curr_x >= 11'd10 && curr_x<= 11'd1269 && curr_y>=10'd10 && curr_y <= 10'd789)
-                counterColumn = counterColumn + 1'd1;
-        end
-    end
-//      always @(posedge clkout) begin
-//        if(rst) counter <= 2'd0;
-//        else if(curr_x >= 11'd10 && curr_x<= 11'd1269  && curr_y>=10'd10 && curr_y <= 10'd789) begin
-//            if (counter == 2'd2)   counter <= 2'd0;  
-//            else counter = counter + 1'b1; 
-//        end
-//      end
-//    /*Show the background*/
-//	always @(posedge clkout) begin
-//		if(rst)
-//			R_rom_addr <= 17'd0;
-//		else begin
-//		    if(R_rom_addr == 17'd109199)
-//		        R_rom_addr = 17'd0;
-//		    else if(curr_x >= 11'd10 && curr_x<= 11'd1269  && curr_y>=10'd10 && curr_y <= 10'd789) begin
-//		        if (!counterRow == 1)  R_rom_addr = R_rom_addr - 420;
-//		        R_rom_addr = R_rom_addr +1'd1;
-//		        if (!counterColumn == 1) R_rom_addr = R_rom_addr - 1'd1;     
-//		    end
-//		  end
-//	   end
-
-/* show the whole image by using address*/
-	always @(posedge clkout) begin
-		if(rst)
-			R_rom_addr <= 17'd0;
-		else begin
-		     if(curr_x >= 11'd10 && curr_x<= 11'd1269  && curr_y>=10'd10 && curr_y <= 10'd789) begin
-		        if(counterRow == 1) begin
-		          if(counterColumn == 1) R_rom_addr = ((curr_y-10)/3 * 420)+ (curr_x - 10)/3;
-		          else if (counterColumn == 2) R_rom_addr = ((curr_y-10)/3 * 420) + ((curr_x - 10) -1)/3;
-		          else if (counterColumn == 0) R_rom_addr = ((curr_y-10)/3 * 420)+ ((curr_x - 10) -2)/3;
-		        end
-		        else if (counterRow == 2) begin
-		          if(counterColumn == 1) R_rom_addr = ((curr_y - 1-10)/3 * 420) + (curr_x - 10)/3;
-		          else if (counterColumn == 2) R_rom_addr = ((curr_y - 1-10)/3 * 420) + ((curr_x - 10) -1)/3;
-		          else if (counterColumn == 0) R_rom_addr = ((curr_y - 1-10)/3 * 420) + ((curr_x - 10) -2)/3;
-		        end
-		        else if (counterRow == 0) begin
-		          if(counterColumn == 1) R_rom_addr = ((curr_y - 2-10)/3 * 420 ) + (curr_x - 10)/3;
-		          else if (counterColumn == 2) R_rom_addr = ((curr_y - 2-10)/3 * 420 ) + ((curr_x - 10) -1)/3;
-		          else if (counterColumn == 0) R_rom_addr = ((curr_y - 2-10)/3 * 420 ) + ((curr_x - 10) -2)/3;
-		        end
-		     end
-		  end
-	end
-      
+    /* get background figure instance*/
+    //for testing, comment this instance
+    bkg_figure bkg (.clk(clkout), .rst(rst), .curr_x(curr_x), .curr_y(curr_y), .addr(R_rom_addr));
+    
+    /* get hook moving positions*/
+    hook_moving moveHook(.clk(clkout), .blkpos_x_out(blk_out_x), .blkpos_y_out(blk_out_y));
+    
+    /* drawcon instance*/ 
+    //for testing, delete comment W_rom_data , use the second row   
+    drawcon drawEnvironment (.W_rom_data(W_rom_data), .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or));
+    //drawcon drawEnvironment ( .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or));  
 endmodule
