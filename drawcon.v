e85d5c4fd487e61cd05e07624ad63fa07e30b4e9
YangZiyi121 <89967414+YangZiyi121@users.noreply.github.com>
2021-11-07 10:17:17 +0300

    Adding golds

--- drawcon.v
+++ drawcon.v
@@ -22,10 +22,13 @@
 
 module drawcon(
     input [7:0] W_rom_data, //for testing, comment this statement
+    input [7:0] W_rom_data_gold0, W_rom_data_gold1,
     input [10:0] draw_x,
     input [9:0] draw_y,
     input [10:0] blkpos_x,
     input [9:0] blkpos_y,
+    input [10:0] x9,x8,x7,x6,x5,x4,x3,x2,x1,x0,
+    input [9:0] y9,y8,y7,y6,y5,y4,y3,y2,y1,y0,
     output [3:0] draw_r,
     output [3:0] draw_g,
     output [3:0] draw_b
@@ -33,7 +36,9 @@ module drawcon(
     
     reg [3:0] bg_r, bg_g, bg_b;
     reg [3:0] blk_r, blk_g, blk_b;
-    
+    reg [3:0] gold_r, gold_g, gold_b;
+
+     
     //Draw border and background
     always @ *
     begin
@@ -62,7 +67,7 @@ module drawcon(
         begin
             blk_r <= 4'b1111;
             blk_g <= 4'b1111;
-            blk_b <= 4'b0000;
+            blk_b <= 4'b1111;
         end
     else 
         begin
@@ -72,10 +77,32 @@ module drawcon(
         end
     end
     
+   //Draw gold
+   always @*begin
+   if (draw_x >= x0 & draw_x<= x0 + 11'd39 & draw_y>=y0 & draw_y<= y0 + 10'd39)
+       begin
+           gold_r <= W_rom_data_gold0[7:5]*2;
+           gold_g <= W_rom_data_gold0[4:2]*2;
+           gold_b <= W_rom_data_gold0[1:0]*4;
+       end
+   else if (draw_x >= x1 & draw_x <= x1 + 11'd39 & draw_y >= y1 & draw_y<= y1 + 10'd39) 
+        begin
+           gold_r <= W_rom_data_gold1[7:5]*2;
+           gold_g <= W_rom_data_gold1[4:2]*2;
+           gold_b <= W_rom_data_gold1[1:0]*4;
+       end     
+   else 
+        begin
+            gold_r <= 4'b0000;
+            gold_g <= 4'b0000;
+            gold_b <= 4'b0000;
+        end
+    end      
+  
     //Assign Output
-    assign draw_r = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_r : bg_r;
-    assign draw_g = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_g : bg_g;
-    assign draw_b = (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_b : bg_b;
+    assign draw_r = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_r :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_r : bg_r;
+    assign draw_g = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_g :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_g : bg_g;
+    assign draw_b = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_b :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_b : bg_b;
 
     
 endmodule
--- game_top.v
+++ game_top.v
@@ -38,13 +38,20 @@ module game_top(
     reg [1:0] counterRow = 2'd0;
     reg clkdraw = 1'b0;
     wire [16:0] R_rom_addr;
+    wire [10:0] R_rom_addr_gold0, R_rom_addr_gold1;
     wire [7:0] W_rom_data;
+    wire [7:0] W_rom_data_gold0,W_rom_data_gold1;
+    wire [10:0] x9,x8,x7,x6,x5,x4,x3,x2,x1,x0;
+    wire [9:0] y9,y8,y7,y6,y5,y4,y3,y2,y1,y0;
+    
     /* pixal clock instance */
     clk_wiz_0 pixClkIns (.clk_in1(clk), .clk_out1(clkout));
     
     /* store background image instance */
     //for testing, comment this instance
     blk_mem_gen_0 rom (.clka(clkout), .addra(R_rom_addr), .douta(W_rom_data));
+    blk_mem_gen_1 rom_gold0 (.clka(clkout), .addra(R_rom_addr_gold0), .douta(W_rom_data_gold0));
+    blk_mem_gen_1 rom_gold1 (.clka(clkout), .addra(R_rom_addr_gold1), .douta(W_rom_data_gold1));
     
     /* vga-out instance */
     vga_out vgainst (.clk(clkout), .rst(rst), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or),                  
@@ -54,11 +61,26 @@ module game_top(
     //for testing, comment this instance
     bkg_figure bkg (.clk(clkout), .rst(rst), .curr_x(curr_x), .curr_y(curr_y), .addr(R_rom_addr));
     
+    goldpositions gold_blk (.clk(clkout), .rst(rst), .curr_x(curr_x), .curr_y(curr_y),
+    .addr0(R_rom_addr_gold0), .addr1(R_rom_addr_gold1),
+    .x9(x9), .x8(x8), .x7(x7), .x6(x6), .x5(x5), .x4(x4), .x3(x3), .x2(x2), .x1(x1), .x0(x0),
+    .y9(y9), .y8(y8), .y7(y7), .y6(y6), .y5(y5), .y4(y4), .y3(y3), .y2(y2), .y1(y1), .y0(y0));
+    
     /* get hook moving positions*/
     hook_moving moveHook(.clk(clkout), .blkpos_x_out(blk_out_x), .blkpos_y_out(blk_out_y));
     
     /* drawcon instance*/ 
-    //for testing, delete comment W_rom_data , use the second row   
-    drawcon drawEnvironment (.W_rom_data(W_rom_data), .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or));
-    //drawcon drawEnvironment ( .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or));  
+    //for testing, comment W_rom_data , use the second row   
+    drawcon drawEnvironment (.W_rom_data(W_rom_data), .W_rom_data_gold0(W_rom_data_gold0),.W_rom_data_gold1(W_rom_data_gold1),
+ .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or), 
+    .x9(x9), .x8(x8), .x7(x7), .x6(x6), .x5(x5), .x4(x4), .x3(x3), .x2(x2), .x1(x1), .x0(x0),
+    .y9(y9), .y8(y8), .y7(y7), .y6(y6), .y5(y5), .y4(y4), .y3(y3), .y2(y2), .y1(y1), .y0(y0));
+//    drawcon drawEnvironment (.W_rom_data_gold0(W_rom_data_gold0),.W_rom_data_gold1(W_rom_data_gold1),
+//     .blkpos_x(blk_out_x), .blkpos_y(blk_out_y),.draw_x(curr_x), .draw_y(curr_y), 
+//     .draw_r(pix_r_or), .draw_g(pix_g_or), .draw_b(pix_b_or),
+//        .x9(x9), .x8(x8), .x7(x7), .x6(x6), .x5(x5), .x4(x4), .x3(x3), .x2(x2), .x1(x1), .x0(x0),
+//        .y9(y9), .y8(y8), .y7(y7), .y6(y6), .y5(y5), .y4(y4), .y3(y3), .y2(y2), .y1(y1), .y0(y0));
+    
+    
+     
 endmodule
