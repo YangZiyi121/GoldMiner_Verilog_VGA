173011ead8e9d2f51f89e3e98efb5cb1c372a9d1
Jihao Xin <xinj@rsws13.kaust.edu.sa>
2021-11-11 11:48:30 +0300

    Signed one-pixle wave rope

--- GoldMiner.srcs/sources_1/imports/GoldMiner_Verilog_VGA/drawcon.v
+++ GoldMiner.srcs/sources_1/imports/GoldMiner_Verilog_VGA/drawcon.v
@@ -29,30 +29,29 @@ module drawcon(
     input [9:0] blkpos_y,
     input [10:0] x9,x8,x7,x6,x5,x4,x3,x2,x1,x0,
     input [9:0] y9,y8,y7,y6,y5,y4,y3,y2,y1,y0,
-    output [3:0] draw_r,
-    output [3:0] draw_g,
-    output [3:0] draw_b
+    output reg [3:0] draw_r,
+    output reg [3:0] draw_g,
+    output reg [3:0] draw_b
     );
     
     reg [3:0] bg_r, bg_g, bg_b;
     reg [3:0] blk_r, blk_g, blk_b;
     reg [3:0] gold_r, gold_g, gold_b;
-
-     
+    reg [3:0] rope_r, rope_g, rope_b;
     //Draw border and background
     always @ *
     begin
     if (draw_x < 11'd10 | draw_x > 11'd1269 | draw_y < 10'd10 | draw_y > 10'd789)
         begin
-            bg_r <=  4'b1111;
-            bg_g <=  4'b1111;
-            bg_b <=  4'b1111;
+            bg_r =  4'b1111;
+            bg_g =  4'b1111;
+            bg_b =  4'b1111;
         end
     else 
         begin
-            bg_r <= W_rom_data[7:5]*2;
-            bg_g <= W_rom_data[4:2]*2;
-            bg_b <= W_rom_data[1:0]*4;
+            bg_r = W_rom_data[7:5]*2;
+            bg_g = W_rom_data[4:2]*2;
+            bg_b = W_rom_data[1:0]*4;
   /*for testing, use the below ones*/
 //            bg_r <=  4'b0000;
 //            bg_g <=  4'b0000;
@@ -60,20 +59,20 @@ module drawcon(
         end
     end
 
-    //Draw block
+    //Draw Hook
     always @*
     begin
     if (draw_x >=blkpos_x & draw_x<=blkpos_x+11'd32 & draw_y >=blkpos_y & draw_y<=blkpos_y+10'd32)
         begin
-            blk_r <= 4'b1111;
-            blk_g <= 4'b1111;
-            blk_b <= 4'b1111;
+            blk_r = 4'b1111;
+            blk_g = 4'b1111;
+            blk_b = 4'b1111;
         end
     else 
         begin
-            blk_r <= 4'b0000;
-            blk_g <= 4'b0000;
-            blk_b <= 4'b0000;
+            blk_r = 4'b0000;
+            blk_g = 4'b0000;
+            blk_b = 4'b0000;
         end
     end
     
@@ -81,28 +80,94 @@ module drawcon(
    always @*begin
    if (draw_x >= x0 & draw_x<= x0 + 11'd39 & draw_y>=y0 & draw_y<= y0 + 10'd39)
        begin
-           gold_r <= W_rom_data_gold0[7:5]*2;
-           gold_g <= W_rom_data_gold0[4:2]*2;
-           gold_b <= W_rom_data_gold0[1:0]*4;
+           gold_r = W_rom_data_gold0[7:5]*2;
+           gold_g = W_rom_data_gold0[4:2]*2;
+           gold_b = W_rom_data_gold0[1:0]*4;
        end
    else if (draw_x >= x1 & draw_x <= x1 + 11'd39 & draw_y >= y1 & draw_y<= y1 + 10'd39) 
         begin
-           gold_r <= W_rom_data_gold1[7:5]*2;
-           gold_g <= W_rom_data_gold1[4:2]*2;
-           gold_b <= W_rom_data_gold1[1:0]*4;
+           gold_r = W_rom_data_gold1[7:5]*2;
+           gold_g = W_rom_data_gold1[4:2]*2;
+           gold_b = W_rom_data_gold1[1:0]*4;
        end     
    else 
         begin
-            gold_r <= 4'b0000;
-            gold_g <= 4'b0000;
-            gold_b <= 4'b0000;
+            gold_r = 4'b0000;
+            gold_g = 4'b0000;
+            gold_b = 4'b0000;
         end
-    end      
+    end
+    
+    //Draw Rope
+    // parameter signed x_left = 10'd560, x_right = 10'd710;
+
+    // reg signed [9:0] x_2, y_2
+   parameter signed x_1 = 15'd635, y_1 = 15'd167; //position of miner, 15 bits to prevent overflow of ((y_0-y_1)*(x_2-x_1)) == ((x_0 - x_1)*(y_2-y_1))
+   parameter signed r = 15'd10; //rope width
+   reg signed [15:0] x_2, y_2,x_0,y_0;
+   always @*
+   begin
+   x_2 = $signed({1'b0,blkpos_x});
+   y_2 = $signed({1'b0,blkpos_y});
+   x_0 = $signed({1'b0,draw_x});
+   y_0 = $signed({1'b0,draw_y});
+   if ((x_0>x_2 & x_0< x_1) | (x_0<x_2 & x_0> x_1))
+   begin
+    if(((y_0-y_1)*(x_2-x_1)) == ((x_0 - x_1)*(y_2-y_1)))
+//      if( (k*(d_x - m_x)+m_y) == d_y )
+       begin
+            rope_r = 4'b0000;
+            rope_g = 4'b1111;
+            rope_b = 4'b1111;
+       end
+       else
+        begin
+            rope_r = 4'b0000;
+            rope_g = 4'b0000;
+            rope_b = 4'b0000;
+        end
+    end
+   else 
+        begin
+            rope_r = 4'b0000;
+            rope_g = 4'b0000;
+            rope_b = 4'b0000;
+        end
+  end
   
+//  end
     //Assign Output
-    assign draw_r = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_r :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_r : bg_r;
-    assign draw_g = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_g :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_g : bg_g;
-    assign draw_b = (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)? gold_b :(blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)? blk_b : bg_b;
+    always @*
+    begin
+        // draw gold
+        if (gold_r > 4'b0000 | gold_g > 4'b0000 | gold_b > 4'b0000)
+        begin
+            draw_r = gold_r;
+            draw_g = gold_g;
+            draw_b = gold_b;
+        end
+        // draw hook
+        else if (blk_r > 4'b0000 | blk_g > 4'b0000 | blk_b > 4'b0000)
+        begin
+            draw_r = blk_r;
+            draw_g = blk_g;
+            draw_b = blk_b;
+        end
+        // draw rope
+        else if (rope_r > 4'b0000 | rope_g > 4'b0000 | rope_b > 4'b0000)
+        begin
+            draw_r = rope_r;
+            draw_g = rope_g;
+            draw_b = rope_b;
+        end
+        // draw backgroung
+        else
+        begin
+            draw_r = bg_r;
+            draw_g = bg_g;
+            draw_b = bg_b;
+        end
+    end
 
     
 endmodule
